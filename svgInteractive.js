4a2f729a5e673a1e9d9b0c389a69ad076da32c63
oreo21 <reok99@gmail.com>
2017-03-09 14:03:44 -0500

    renamed

---
+++ svgInteractive.js
@@ -0,0 +1,8 @@
+var frame = document.getElementById("frame");
+var cButton = document.getElementById("clear");
+var mButton = document.getElementById("move");
+var e = window.event;
+
+var drawCircle = function(x, y){
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+}
